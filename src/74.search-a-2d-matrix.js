755426920811ab7a4836ac8effda5f0c36d76071
liujia <liujia30@jd.com>
2018-05-12 17:13:53 +0800

    done 74 binary search

--- src/74.search-a-2d-matrix.js
+++ src/74.search-a-2d-matrix.js
@@ -6,13 +6,59 @@
 var searchMatrix = function(matrix, target) {
     // 采用二分法，时间复杂度O(log(MN))
     
-    let rowLen = matrix.length;
-    let colLen = matrix[0].length;
+    const rowLen = matrix.length;
+    if (rowLen === 0) return false;
+    const colLen = matrix[0].length;
+    if (colLen === 0) return false;
 
-    let p = Math.floor(rowLen * colLen / 2);
-    
-    let row = Math.floor(p / rowLen);
-    let col = p % rowLen;
-    
+    const nums = rowLen * colLen;
 
+    let lp = 0, rp = nums - 1;
+
+    function getRow(p) {
+        return Math.floor(p / colLen);
+    }
+    function getCol(p) {
+        return p % colLen;
+    }
+
+    let lp_row = getRow(lp),
+        lp_col = getCol(lp),
+        rp_row = getRow(rp),
+        rp_col = getCol(rp);
+
+    while(lp < rp && matrix[lp_row][lp_col] <= target && matrix[rp_row][rp_col] >= target) {
+        if (matrix[lp_row][lp_col] === target || matrix[rp_row][rp_col] === target) {
+            return true;
+        }
+
+        let mid = Math.floor((lp + rp) / 2);
+
+        // 避免lp === mid 死循环
+        if (mid === lp || mid === rp) {
+            return false;
+        }
+
+        let midValue = matrix[getRow(mid)][getCol(mid)];
+        if (midValue > target) {
+            rp = mid;
+        } else if (midValue < target) {
+            lp = mid;
+        } else {
+            return true;
+        }
+    }
+
+    // lp === rp时判断
+    lp_row = getRow(lp),
+    lp_col = getCol(lp),
+    rp_row = getRow(rp),
+    rp_col = getCol(rp);
+    if (matrix[lp_row][lp_col] === target || matrix[rp_row][rp_col] === target) {
+        return true;
+    }
+
+    return false;
 };
+
+module.exports = searchMatrix;
--- test/array.js
+++ test/array.js
@@ -111,3 +111,32 @@ describe('64.minimum-path-sum', function () {
         expect(minPathSum(target)).to.eql(7);
     });
 });
+
+describe('74.search-a-2d-matrix', function () {
+    const searchMatrix = require('../src/74.search-a-2d-matrix');
+    it('matrix [[1,3,5,7],[10,11,16,20],[23,30,34,50]], target 3, return true', function () {
+        let matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,50]];
+        let target = 3;
+        expect(searchMatrix(matrix, target)).to.eql(true);
+    });
+    it('matrix [[1,3]], target 2, return false', function () {
+        let matrix = [[1,3]];
+        let target = 2;
+        expect(searchMatrix(matrix, target)).to.eql(false);
+    });
+    it('matrix [[1]], target 2, return false', function () {
+        let matrix = [[1]];
+        let target = 2;
+        expect(searchMatrix(matrix, target)).to.eql(false);
+    });
+    it('matrix [[1]], target 1, return true', function () {
+        let matrix = [[1]];
+        let target = 1;
+        expect(searchMatrix(matrix, target)).to.eql(true);
+    });
+    it('matrix [[1,3,5,7],[10,11,16,20],[23,30,34,50]], target 15, return false', function () {
+        let matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,50]];
+        let target = 15;
+        expect(searchMatrix(matrix, target)).to.eql(false);
+    });
+});
